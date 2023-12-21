37a54004a588a28ffa865e1c716e180b92cf8271
young-win <young.zhu.2012@gmail.com>
2023-12-21 10:53:08 +0800

    basic example

---
+++ basic/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	// Instantiate default collector
+	c := colly.NewCollector(
+	// Visit only domains
+	// domain 要慎用！！
+	// http://www.youngzy.com/ 这个居然不满足下面的域
+	// 找了其他网站看看，很多都省略 www 了
+	//colly.AllowedDomains("youngzy.com", "github.com"),
+	)
+
+	// On every an element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	// Start scraping
+	c.Visit("https://www.youngzy.com/")
+
+}
+
+func _main() {
+	// Instantiate default collector
+	c := colly.NewCollector(
+		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
+		colly.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"),
+	)
+
+	// On every a element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	// Start scraping on https://hackerspaces.org
+	c.Visit("https://hackerspaces.org/")
+}
